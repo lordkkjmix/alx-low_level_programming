54b1db2c3389b4e1f01dfff246521d73cc2ee224
kouakou koffi josue <lordkkjmix@gmail.com>
2022-10-18 20:54:32 +0000

    Add comments

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,11 @@
 #include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of jack_bauer,
+ * starting from 00:00 to 23:59.
+ *
+ * Return: void.
+ */
 void jack_bauer(void)
 {
 	int h, m;
